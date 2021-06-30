172319e6eda16930ea7e1aeda6e6a7810ee34cfa
mrandolph83 <mrandolph83@gmail.com>
2021-06-30 15:37:59 -0400

    Made user create path

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :reviews, through: :jobs
     has_many :employees, through: :jobs
 
+    validates :email, uniqueness: true, presence: true
+    has_secure_password
 end
