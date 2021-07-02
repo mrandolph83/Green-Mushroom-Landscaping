5d131cd93dd1a0b3227aec8ee76399fd7a110350
mrandolph83 <mrandolph83@gmail.com>
2021-07-02 16:53:54 -0400

    Basic job controller/view functionality, needs form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,28 @@
 class JobsController < ApplicationController
+
+    def new
+        @job = Job.new
+    end
+
+    def create
+      @job = job.new(job_params)
+      if @job.save 
+        session[:job_id] = @job.id 
+        redirect_to job_path(@job)
+      else 
+        render :new
+      end 
+    end
+
+    def show
+        @job = job.find_by_id(params[:id])
+        redirect_to '/' if !@job
+    end
+
+    private
+
+    def job_params
+        params.require(:job).permit(:email, :password)
+    end 
+
 end
