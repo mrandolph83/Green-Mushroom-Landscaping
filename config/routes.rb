6dc1720f2aa7b595735ee0f5bb8c081bf5be4169
mrandolph83 <mrandolph83@gmail.com>
2021-07-06 15:43:31 -0400

    Took out unnecessary jobs route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
   resources :reviews
-  resources :jobs
   resources :jobs do
     resources :reviews, only: [:new, :index]
     end
