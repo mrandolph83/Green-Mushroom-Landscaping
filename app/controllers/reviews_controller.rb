a5fc38e7af59d93d3ea69d40268f309e5da05706
mrandolph83 <mrandolph83@gmail.com>
2021-07-08 13:15:49 -0400

    Nested reviews routes, functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,37 @@
 class ReviewsController < ApplicationController
 
 def new 
-    @job = Job.find_by_id(params[:id])
+    @job = Job.find_by_id(params[:job_id])
     @review = @job.reviews.build
 end 
 
-def index
-    @reviews = Review.all
+def create
+    @review = Review.new(review_params)
+    
+    if @review.save 
+        redirect_to review_path(@review)
+    else 
+        render :new 
+    end 
+end
+
+def show
+    @review = Review.find_by_id(params[:id])
+
 end 
+
+def index
+    if params[:job_id]
+        @job = Job.find_by_id(params[:job_id])
+        @reviews = @job.reviews
+    else
+        @reviews = Review.all
+end 
+
+private
+
+def review_params
+    params.require(:review).permit(:job_id, :rating, :description)  
+end 
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
 belongs_to :job
 
+validates :rating, presence: true
 
 
 end
